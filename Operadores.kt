eaefbfd60a84924c2acdd71b971cf1e329bcae36
Angel Daniel Vázquez Esqueda <103538268+AngelVazquezEsqueda@users.noreply.github.com>
2023-09-17 22:03:05 -0600

    Create Operadores.kt

---
+++ Operadores.kt
@@ -0,0 +1,14 @@
+fun main() {
+   
+   //Calculo de una pendiente
+   val x1=4.0
+   val x2=-3.0
+   val y1=3.0
+   val y2=-2.0
+    
+   val m = (y2-y1) / (x2-x1)
+   
+   println("Calculo de una pendiente entre las coordenadas ($x1, $y1) y ($x2, $y2):")
+   println("La pendiente es: $m")
+   
+}
